79e8c97c411eeeb3e992c849d14fbc2e64cdae14
Logon Aluno <logonrmlocal@fiap.com.br>
2019-02-14 09:50:19 -0200

    atualizacao

---
+++ 03-JPA-Introducao/src/br/com/erosmendonca/entity/Carro.java
@@ -0,0 +1,27 @@
+package br.com.erosmendonca.entity;
+
+import java.util.Calendar;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TB_CARRO")
+@SequenceGenerator(name="carro",sequenceName="SQ_TB_CARRO", allocationSize=1)
+public class Carro {
+	@Id
+	@GeneratedValue(generator="carro",strategy=GenerationType.SEQUENCE)
+	@Column(name="CD_CODIGO")
+	private int codigo;
+	private String nome;
+	private String marca;
+	private String placa;
+	private int ano;
+	private Transmissão cambio;
+	private Calendar dataFabricacao;		
+}
