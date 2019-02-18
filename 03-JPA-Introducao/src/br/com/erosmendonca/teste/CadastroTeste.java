4c481dacfe22985735ac14ae0f2622567764debf
Logon Aluno <logonrmlocal@fiap.com.br>
2019-02-18 11:57:34 -0300

    crud

--- 03-JPA-Introducao/src/br/com/erosmendonca/teste/CadastroTeste.java
+++ 03-JPA-Introducao/src/br/com/erosmendonca/teste/CadastroTeste.java
@@ -1,10 +1,16 @@
 package br.com.erosmendonca.teste;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class Teste {
+import br.com.erosmendonca.entity.TipoZoologico;
+import br.com.erosmendonca.entity.Zoologico;
+
+public class CadastroTeste {
 	public static void main(String[] args) {
 		//Criar um gerenciador de entidades (Entity Manager)
 		//Primeiro criar a fabrica de entity manager
@@ -12,6 +18,15 @@ public class Teste {
 				Persistence.createEntityManagerFactory("CLIENTE_ORACLE");
 		//Depois, a fabrica cria o entity manager
 		EntityManager em = fabrica.createEntityManager();
+		//Cadastrar um zoo
+		Zoologico zoo = new Zoologico(
+				"Zoo", 10,TipoZoologico.SILVESTRE, new Date(), 
+				new Date(), Calendar.getInstance(), false, null
+				);
+		
+		em.persist(zoo);
+		em.getTransaction().begin();
+		em.getTransaction().commit();
 		
 		em.close();
 		fabrica.close();		
